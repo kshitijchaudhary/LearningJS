14ba8e4693aab7e237165e91fb5d3931315e5728
kshitijchaudhary <chaudharykshitij49@gmail.com>
2024-03-02 13:21:45 -0500

    Javascript Classes and Expressions

--- script.js
+++ script.js
@@ -48,3 +48,88 @@
 
 
 //Getters and Setters in Javascript
+/* Getters and setters are special methods in javascript that
+allow you to control how properties are accessed and modified.
+Defined using get and set keyword */
+
+//Getter method
+/* A getter is a method which is called when a property is accessed.
+ It can be used to do things like validate the value of property
+ or convert it to a different format.*/
+
+//  class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     get greet(){
+//         return "Hello " + this.name;
+//     }
+//  }
+
+//  const person1 = new Person("Kshitij Chaudhary", 25);
+//  console.log(person1.greet);
+
+//Setter Method
+/* A setter is a method that is called when a property is modified.
+It can be used to do things like update the value of property
+or perform some other action 
+We can use same method as getter and setter*/
+// class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     set changeName(newName){
+//         this.name = newName;
+//     }
+// }
+
+//  const person1 = new Person("Kshitij Chaudhary", 25);
+
+//  person1.changeName =  "Xitol";
+//  console.log(person1);
+
+
+//example
+// class Person{
+//     constructor(name, age){
+//         this.name = name;
+//         this.age = age;
+//     }
+//     set personName(newName){
+//         this.name = newName;
+//     }
+//     get personName(){
+//         return this.name;
+//     }
+// }
+
+//  const person1 = new Person("Kshitij Chaudhary", 25);
+
+//  console.log(person1.personName);
+//  person1.personName = "Xitol";
+//  console.log(person1.personName);
+
+//Javascript Class Expression
+/* A class expression provides you an alternative way to
+ define a new class.
+ It is similar to function expression, but it uses the class
+ keyword instead of the function keyword.
+ Class expressions can be named or unnamed.
+ If they are named, the name can be used to refer the class later.
+  If they are unnamed, they can only be referred by the variable that
+  they are assigned of.*/
+
+  let Person = class{
+    constructor(name){
+        this.name = name
+    }
+    getName(){
+        return this.name
+    }
+  }
+
+  const person1 = new Person("Kshitij Chaudhary");
+
+  console.log(person1);
